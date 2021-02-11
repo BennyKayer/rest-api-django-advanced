37386181d1e73e95a7e7f966c5efec30a11e0205
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-02-11 13:39:37 +0100

    7 Finished

--- app/core/models.py
+++ app/core/models.py
@@ -10,7 +10,8 @@ class UserManager(BaseUserManager):
     """Manages user actions
 
     Args:
-        BaseUserManager (BaseUserManager): https://docs.djangoproject.com/en/3.1/topics/auth/customizing/#django.contrib.auth.models.BaseUserManager
+        BaseUserManager (BaseUserManager):
+        https://docs.djangoproject.com/en/3.1/topics/auth/customizing/#django.contrib.auth.models.BaseUserManager
     """
 
     def create_user(self, email, password=None, **extra_fields):
@@ -20,10 +21,27 @@ class UserManager(BaseUserManager):
             email (str): User email
             password (string, optional): User's password. Defaults to None.
         """
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
         # Manager has access to underlying model
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         # good practice, supports multiple databases
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user
+
+        Args:
+            email (str): Superuser's email
+            password (str): Superuser's password
+        """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -33,8 +51,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username
 
     Args:
-        AbstractBaseUser (AbstractBaseUser): https://docs.djangoproject.com/en/3.1/topics/auth/customizing/#django.contrib.auth.models.AbstractBaseUser
-        PermissionsMixin (PermissionsMixin): https://docs.djangoproject.com/en/3.1/topics/auth/customizing/#django.contrib.auth.models.PermissionsMixin
+        AbstractBaseUser (AbstractBaseUser):
+        https://docs.djangoproject.com/en/3.1/topics/auth/customizing/#django.contrib.auth.models.AbstractBaseUser
+        PermissionsMixin (PermissionsMixin):
+        https://docs.djangoproject.com/en/3.1/topics/auth/customizing/#django.contrib.auth.models.PermissionsMixin
     """
 
     email = models.EmailField(max_length=255, unique=True)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,30 @@ class ModelTests(TestCase):
         """Test creating new user with email"""
         email = "test@gmail.com"
         password = "p455WORDC#"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email, password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test whether email for new user has been normalized"""
+        email = "test@GMAIL.COM"
+        user = get_user_model().objects.create_user(email, "test1234")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_valid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "test1234")
+
+    def test_create_new_superuser(self):
+        """Test creating new super user"""
+        user = get_user_model().objects.create_superuser(
+            "test@GMAIL.com", "test213"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
