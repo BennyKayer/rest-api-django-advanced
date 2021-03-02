d2f20bc17763bddc56726b9a876e088bc88e22a0
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-03-02 17:32:57 +0100

    up to 54 Add feature to list tags

---
+++ app/recipe/views.py
@@ -0,0 +1,28 @@
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Tag
+from recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage tag in the database
+
+    Args:
+        viewsets ([type]): [description]
+        mixins ([type]): [description]
+    """
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only
+
+        Returns:
+            [type]: [description]
+        """
+        return self.queryset.filter(user=self.request.user).order_by("-name")
--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -24,3 +24,23 @@ class CreateTokenView(ObtainAuthToken):
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user
+
+    Args:
+        generics ([type]): [description]
+    """
+
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return autheticated user
+
+        Returns:
+            [type]: [description]
+        """
+        return self.request.user
